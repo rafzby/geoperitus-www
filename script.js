5dc1e7e04b6fa0d9839fc47d38156de8516237ef
Rafal Zbytniewski <rafal.zbytniewski@nephilim.software>
2020-05-13 20:13:28 +0200

    add mobile menu mechanism

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("mobileMenu");
+
+    if (menu.classList.contains("u-hidden")) {
+        menu.classList.remove("u-hidden")
+    } else {
+        menu.classList.add("u-hidden")
+    }
+  } 
