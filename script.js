53259b49ca29c5c5257dec365494fbbf06ef303d
Rafal Zbytniewski <rafal.zbytniewski@nephilim.software>
2020-05-13 21:05:56 +0200

    Fix sections padding

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function toggleMenu() {
+function toggleMobileMenu() {
     var menu = document.getElementById("mobileMenu");
-    
+
     if (menu.classList.contains("u-hidden")) {
         menu.classList.remove("u-hidden")
     } else {
         menu.classList.add("u-hidden")
     }
-  } 
+}
+
+
+(function () {
+    document.querySelectorAll('.c-mobile-menu__item').forEach(item => {
+        item.addEventListener('click', event => {
+            toggleMobileMenu()
+        })
+    })
+})();
+
