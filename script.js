16520333d90e5440125579a6ae5f6a1e7e9b84f7
Rafal Zbytniewski <rafal.zbytniewski@nephilim.software>
2020-05-13 20:14:58 +0200

    Update scripts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     var menu = document.getElementById("mobileMenu");
-
+    
     if (menu.classList.contains("u-hidden")) {
         menu.classList.remove("u-hidden")
     } else {
