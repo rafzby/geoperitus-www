fddb8af521ceb5e036879347cefc326c759c977a
Rafal Zbytniewski <rafal.zbytniewski@nephilim.software>
2020-05-15 21:24:00 +0200

    Outline mobile menu hamburger

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 function toggleMobileMenu() {
     var menu = document.getElementById("mobileMenu");
+    var hamburger = document.getElementById("mobileHamburger")
 
     if (menu.classList.contains("u-hidden")) {
         menu.classList.remove("u-hidden")
+        hamburger.classList.add("c-hamburger--active")
     } else {
         menu.classList.add("u-hidden")
+        hamburger.classList.remove("c-hamburger--active")
     }
 }
 
